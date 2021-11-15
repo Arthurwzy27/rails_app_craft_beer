3f87be05e55c0c7dfb3a64d5a230f420bc5fd4c3
Arthur Picard <arthur.picard7@gmail.com>
2021-11-15 15:56:53 +0100

    Created one factory for Factory show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Factory.create!(
+  name: "Factory 1",
+  description: "This is the description of factory number 1",
+  location: "Barcelona",
+  price_per_day: 32,
+  capacity: 10,
+  user_id: User.first.id
+)
