9e51defaa837990f605e1745cd43a3eb2c1639de
Arthur Picard <arthur.picard7@gmail.com>
2021-11-15 15:07:20 +0100

    Link routes path and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_133609) do
+ActiveRecord::Schema.define(version: 2021_11_15_134234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "factory_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["factory_id"], name: "index_beers_on_factory_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_start"
+    t.bigint "factory_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["factory_id"], name: "index_bookings_on_factory_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "factories", force: :cascade do |t|
     t.string "name"
@@ -37,4 +57,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_133609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "beers", "factories"
+  add_foreign_key "bookings", "factories"
+  add_foreign_key "bookings", "users"
 end
