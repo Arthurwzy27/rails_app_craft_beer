c8404dcaad2afa2bd823e6992c6c38084abd75c6
Arthur Picard <arthur.picard7@gmail.com>
2021-11-17 11:27:15 +0100

    Features: New, create and factory_params method created + Create factory page views created

--- app/controllers/factories_controller.rb
+++ app/controllers/factories_controller.rb
@@ -8,4 +8,25 @@ class FactoriesController < ApplicationController
     @factory = Factory.find(params[:id])
     authorize @factory
   end
+
+  def new
+    @factory = Factory.new
+    authorize @factory
+  end
+
+  def create
+    @factory = Factory.new(factory_params)
+    if @factory.save
+      redirect_to factory_path(@factory)
+    else
+      render :new
+    end
+    authorize @factory
+  end
+
+  private
+
+  def factory_params
+    params.require(:factory).permit(:name, :description, :location, :price_per_day, :capacity, :user_id)
+  end
 end
