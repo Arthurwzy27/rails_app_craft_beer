99ad1fe889987c4cd8180c3295c897a31f6be7cd
Arthur Picard <arthur.picard7@gmail.com>
2021-11-17 14:56:46 +0100

    Map layout reposition

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,9 +1,11 @@
 class BeersController < ApplicationController
   def index
     @beers = Beer.all
+    authorize @beers
   end
 
   def show
     @beer = Beer.find(params[:id])
+    authorize @beer
   end
 end
