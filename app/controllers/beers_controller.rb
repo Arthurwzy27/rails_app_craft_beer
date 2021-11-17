9569b68f9fa4386d5765987cf68d07fe4f618d95
Arthur Picard <arthur.picard7@gmail.com>
2021-11-17 16:24:23 +0100

    Update: Link Factories to correct factory path + display created factory on factories index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,11 +1,9 @@
 class BeersController < ApplicationController
   def index
     @beers = Beer.all
-    authorize @beers
   end
 
   def show
     @beer = Beer.find(params[:id])
-    authorize @beer
   end
 end
