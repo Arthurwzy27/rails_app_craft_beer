d61a9bb3d95a955e6e72a421ff94f12c45937fe7
Arthur Picard <arthur.picard7@gmail.com>
2021-11-18 16:34:35 +0100

    User db and factory db created and fix

---
+++ db/migrate/20211113156000_create_factories.rb
@@ -0,0 +1,14 @@
+class CreateFactories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :factories do |t|
+      t.string :name
+      t.text :desription
+      t.string :location
+      t.integer :price_per_day
+      t.integer :capacity
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
