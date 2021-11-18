5ef13ef34e602bd9203d7f471c107dd5dad6d3ad
Arthur Picard <arthur.picard7@gmail.com>
2021-11-18 13:02:54 +0100

    Booking info layout display flex

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,12 @@
 // app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr("#range_start", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#range_end"})]
+  });
 }
 
 export { initFlatpickr };
