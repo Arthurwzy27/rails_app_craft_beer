62e757dd6caf15aff31be30db0f641570e272670
Arthur Picard <arthur.picard7@gmail.com>
2021-11-19 11:21:10 +0100

    Cloudinary adding picture

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,5 +1,6 @@
 class Factory < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description,
     against: [ :name, :description ],
