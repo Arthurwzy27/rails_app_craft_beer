609e14f4ac95e7c3a06ce585c07910e5cb85cad8
Arthur Picard <arthur.picard7@gmail.com>
2021-11-19 16:39:45 +0100

    Fixed map, created factory seed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
+  // console.log(mapElement)
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
