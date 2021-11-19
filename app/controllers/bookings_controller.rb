b2fcf7fa012ca3d24dcd3d5a2d9fb15bdc1f87bf
Arthur Picard <arthur.picard7@gmail.com>
2021-11-19 13:01:55 +0100

    Booking controller created and method new, create and show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,43 @@
 class BookingsController < ApplicationController
+  before_action :set_total_price
+
+  def new
+    @booking = Booking.new
+    @location = Location.find(params[:location_id])
+    authorize @booking
+    authorize @location
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.location = Location.find(params[:location_id])
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    if @booking.update(status: params[:status])
+      redirect_to dashboard_path
+    end
+  end
+
+  def set_total_price
+    self.total_price = (self.end_date - self.start_date).to_i * self.factory.price
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :factory
   belongs_to :user
 
+
   # I can do @booking.accepted!, @booking.declined!, etc.
   enum status: [ :pending, :accepted, :declined ]
 end
